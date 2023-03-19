b6fe1a12d4048c038ca4b9bbef58813be8d746c2
Jake Samuels <samuels242@googlemail.com>
2023-03-19 12:33:33 +0000

    src/

---
+++ src/assets/categories/index.js
@@ -0,0 +1,17 @@
+import deckbuilding from "./deck-building.webp";
+import dexterity from "./dexterity.webp";
+import enginebuilding from "./engine-building.webp";
+import hiddenroles from "./hidden-roles.jpeg";
+import pushyourluck from "./push-your-luck.webp";
+import rollandwrite from "./roll-and-write.jpeg";
+import strategy from "./strategy.webp";
+
+export const images = {
+  deckbuilding,
+  dexterity,
+  enginebuilding,
+  hiddenroles,
+  pushyourluck,
+  rollandwrite,
+  strategy,
+};

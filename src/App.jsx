003a017f4ba2e3e23f9d5458e906ce12a80472e8
Jake Samuels <samuels242@googlemail.com>
2023-03-19 15:36:49 +0000

    Create route for Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { getAllCategories } from "./utils/api";
+
+import Home from "./pages/Home";
+
 const App = () => {
-  return <div></div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getAllCategories().then((categoryData) => {
+      setCategories(categoryData);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home categories={categories} />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;

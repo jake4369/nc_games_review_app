37aca701bac2228dabd654d746fe0350a0206eaa
Jake Samuels <samuels242@googlemail.com>
2023-03-20 00:11:35 +0000

    Add route for single review

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { getAllCategories } from "./utils/api";
+import { getAllCategories, getAllReviews } from "./utils/api";
 
 import Home from "./pages/Home";
+import Reviews from "./pages/Reviews";
+import SingleReview from "./pages/SingleReview";
 
 const App = () => {
   const [categories, setCategories] = useState([]);
+  const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
-    getAllCategories().then((categoryData) => {
-      setCategories(categoryData);
+    getAllCategories().then((categorysData) => {
+      setCategories(categorysData);
+    });
+  }, []);
+
+  useEffect(() => {
+    getAllReviews().then((reviewsData) => {
+      setReviews(reviewsData.data.reviews);
     });
   }, []);
 
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home categories={categories} />} />
+        <Route
+          path="/"
+          element={<Home categories={categories} reviews={reviews} />}
+        />
+        <Route path="/reviews" element={<Reviews reviews={reviews} />} />
+        <Route path="/reviews/:review_id" element={<SingleReview />} />
       </Routes>
     </div>
   );

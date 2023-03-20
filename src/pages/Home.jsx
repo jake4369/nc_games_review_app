de44ee2b8b5c2a67c698fa6e526a2e759d0b72f3
Jake Samuels <samuels242@googlemail.com>
2023-03-20 00:12:25 +0000

    Add snap-scroll carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,46 @@
+import { Link } from "react-router-dom";
+
 import Hero from "./../components/shared/Hero";
 import CategoryCard from "../components/CategoryCard";
+import ReviewCard from "../components/shared/ReviewCard";
 
 import heroImg from "./../assets/goblins.jpeg";
 import { images } from "./../assets/categories/index";
 import mapImg from "./../assets/map.jpeg";
 
-const Home = ({ categories }) => {
-  const categoryCards = categories.map((category) => {
+const Home = ({ categories, reviews }) => {
+  const categoryCards = [...categories].map((category) => {
     const categoryName = category.slug.split("-").join(" ");
     return (
       <CategoryCard
-        key={category.slug}
+        key={category.description}
         categoryImg={images[categoryName.split(" ").join("")]}
         categoryName={categoryName}
         categoryDescription={category.description}
+      />
+    );
+  });
+
+  const reviewCardStyles = {
+    flex: "0 0 80%",
+    maxWidth: "20.3125rem",
+    margin: "0 0.625rem",
+    scrollSnapAlign: "center",
+  };
+
+  const reviewCards = [...reviews].slice(0, 5).map((review) => {
+    return (
+      <ReviewCard
+        key={review.review_id}
+        review_id={review.review_id}
+        img={review.review_img_url}
+        title={review.title}
+        category={review.category}
+        owner={review.owner}
+        created_at={review.created_at}
+        comment_count={review.comment_count}
+        votes={review.votes}
+        style={reviewCardStyles}
       />
     );
   });
@@ -39,7 +66,20 @@ const Home = ({ categories }) => {
       <img src={mapImg} alt="" className="homepage__map-img" />
 
       <section className="homepage__reviews-section">
-        <h2 className="homepage__reviews-section__heading">Latest Reviews</h2>
+        <div className="section__flex-container">
+          <h2 className="homepage__reviews-section__heading">Reviews</h2>
+          <Link to="/reviews">
+            <button className="link-btn view-all-reviews-btn__desktop">
+              View All
+            </button>
+          </Link>
+        </div>
+        <div className="homepage__review-cards__container">{reviewCards}</div>
+        <Link to="/reviews">
+          <button className="link-btn view-all-reviews-btn__mobile">
+            View All
+          </button>
+        </Link>
       </section>
     </div>
   );
